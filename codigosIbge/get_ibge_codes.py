90c2b50c04bfb21329f04ff10bde9bb8fe6b8995
Amanda Jones <101410608+jonesamandajones@users.noreply.github.com>
2023-05-10 11:33:40 -0300

    Update get_ibge_codes.py

--- codigosIbge/get_ibge_codes.py
+++ codigosIbge/get_ibge_codes.py
@@ -1,5 +1,6 @@
-import requests
+import pandas as pd
 import json
+import requests
 
 
 regioes = [1, 2, 3, 4, 5]
@@ -10,3 +11,19 @@ for regiao in regioes:
 
     with open(f'dados_ibge/dados_ibge_regiao={regiao}.json', 'w') as f:
         f.write(json.dumps(r.json(), ensure_ascii=False, indent=4))
+        
+data = []
+for regiao in regioes:
+    key = f'dados_ibge/dados_ibge_regiao={regiao}.json'
+
+    with open(key, 'r') as ibge_data:
+
+        data_dict = json.loads(ibge_data.read())
+        index = len(data_dict) - 1
+
+        for i in range(index):
+            data.append(data_dict[i]['id'])
+
+print(data)
+df = pd.DataFrame(data)
+csv_data = df.to_csv('ibge_codes.csv', index=False)
